a901f7606a26c37367ade2bd51cd5f38e98a0b4c
Ryan Wiener <ryanl.wiener@yahoo.com>
2019-10-26 16:06:38 -0700

    Fix homepage

---
+++ precache-manifest.f3172fcfd471450718237370bcfe8de6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f191401e7bec921b3c342d2ec223638",
+    "url": "/basic_chat_app/index.html"
+  },
+  {
+    "revision": "8a37ccc82a504326c34f",
+    "url": "/basic_chat_app/static/css/main.e8ce65e9.chunk.css"
+  },
+  {
+    "revision": "264f1e0653fbb4dbd085",
+    "url": "/basic_chat_app/static/js/2.8e3975aa.chunk.js"
+  },
+  {
+    "revision": "8a37ccc82a504326c34f",
+    "url": "/basic_chat_app/static/js/main.7d6d30ca.chunk.js"
+  },
+  {
+    "revision": "53f98dbd1e099c1c0b7b",
+    "url": "/basic_chat_app/static/js/runtime-main.7bc39968.js"
+  }
+]);
